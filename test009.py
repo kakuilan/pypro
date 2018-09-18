ebef905e2299e2db8d84c5a31fdd6f0702b83a8a
kakuilan <kakuilan@163.com>
2018-09-18 12:52:59 +0800

    add test008/test009 def function

---
+++ test009.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#函数定义def
+def area(width, height):
+  return width * height
+def print_welcome(name):
+  print('Welcom ', name)
+
+print_welcome('Fred')
+w = 4
+h = 5
+print("Width =", 2, " Heigh =", h, " Area =", area(w,h))
