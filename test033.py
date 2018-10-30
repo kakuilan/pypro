6604d1655228343a3d3de64a462b40701a0cce40
kakuilan <kakuilan@163.com>
2018-10-30 12:37:11 +0800

    if else

---
+++ test033.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# coding: UTF-8
+# if else语句
+
+#if嵌套
+num = float(input('输入一个数字: '))
+if num >=0:
+  if num ==0:
+    print('零')
+  else:
+    print('正数')
+else:
+  print('负数')
