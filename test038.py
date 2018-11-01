b7bf0c6f114b54046311c3225a8424320f8d4e82
kakuilan <kakuilan@163.com>
2018-11-01 12:42:42 +0800

    判断是否质数

---
+++ test038.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# coding: UTF-8
+# 判断是否质数
+
+num = int(input('请输入一个数字: '))
+
+if num >1:
+  for i in range(2,num):
+    if(num%i)==0:
+      print(num, " 不是质数")
+      print(i,"乘于", num//i, "是", num)
+      break
+  else:
+    print(num, " 是质数")
+else:
+  print(num, " 不是质数")
