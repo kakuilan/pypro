1a7d86b99d751db084fdab538d283d83a06da378
kakuilan <kakuilan@163.com>
2018-11-07 12:43:16 +0800

    十进制转二进制、八进制、十六进制

---
+++ test044.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# coding: UTF-8
+# 十进制转二进制、八进制、十六进制
+
+dec = int(input('输入数字: '))
+
+print('十进制数为：', dec)
+print('转换为二进制为：', bin(dec))
+print('转换为八进制为：', oct(dec))
+print('转换为十六进制为：', hex(dec))
