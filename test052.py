af51e5963ba0e44e978d4878ee5ea87a47259387
kakuilan <kakuilan@163.com>
2018-11-09 12:43:46 +0800

    字符串判断

---
+++ test052.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# coding: UTF-8
+# 字符串判断
+
+print('测试实例一')
+str = 'google.com'
+print(str.isalnum()) #判断所有字符都是数字或字母
+print(str.isalpha()) #判断所有字符都是字母
+print(str.isdigit()) #判断所有字符都是数字
+print(str.islower()) #判断所有字符都是小写
+print(str.isupper()) #判断所有字符都是大写
+print(str.istitle()) #判断所有单词都是首字母大写,像标题
+print(str.isspace()) #判断所有字符都是空白字符\t\n\r
+
