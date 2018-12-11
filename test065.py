074bc4a97768ca0f1663c64b6ee3d60e69c2b126
kakuilan <kakuilan@163.com>
2018-12-11 12:48:47 +0800

    elif子句

---
+++ test065.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# coding: utf-8
+# elif子句
+
+num = int(input('Enter a number:'))
+if num >0 :
+  print('The number is prositive')
+elif num < 0:
+  print('The number is negative')
+else:
+  print('The number is zero')
