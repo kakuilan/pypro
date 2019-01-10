205ac7357bcbf49320b9a421167dee2aa30b70bf
kakuilan <kakuilan@163.com>
2019-01-10 12:39:46 +0800

    类-私有属性

---
+++ test076.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# coding: utf-8
+# 类-私有属性
+
+class Secretive:
+  def __inacessible(self):
+    print("Bet you can`t see me ...")
+
+  def accessible(self):
+    print("The secret message is:")
+    self.__inacessible()
+
+s = Secretive()
+s.accessible()
