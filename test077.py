b64b9fe554a00877585da065c2fe47254e4e2bbe
kakuilan <kakuilan@163.com>
2019-01-14 12:46:27 +0800

    超类

---
+++ test077.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# coding: utf-8
+# 超类
+
+class Filter:
+  def init(self):
+   self.blocked = []
+  def filter(self, sequence):
+    return [x for x in sequence if x not in self.blocked]
+
+#SPAMFilter是Filter的子类
+class SPAMFilter(Filter):
+  #重写超类Filter的方法init
+  def init(self):
+    self.blocked = ['SPAM']
+
+f = Filter()
+f.init()
+print(f.filter([1,2,3]))
+
+s = SPAMFilter()
+s.init()
+print(s.filter(['SPAM','SPAM','SPAM','eggs','bacon','SPAM']))
+
