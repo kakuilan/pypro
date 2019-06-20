c83f13b437163d3ff751207db3770e029159ae7f
kakuilan <kakuilan@163.com>
2019-06-20 12:25:19 +0800

    使用Beautiful Soup处理html

---
+++ test123.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# coding: utf-8
+# 使用Beautiful Soup处理html
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+text = urlopen('http://python.org/jobs').read()
+soup = BeautifulSoup(text, 'html.parser')
+
+jobs = set()
+for job in soup.body.section('h2'):
+  jobs.add('{} ({})'.format(job.a.string, job.a['href']))
+
+print('\n'.join(sorted(jobs, key=str.lower)))
